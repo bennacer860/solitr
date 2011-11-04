5df992b2b5cfc79b52b4498abf4258be41607f29
Jo Liss <joliss42@gmail.com>
2011-11-04 10:41:16 +0100

    new Rails 3.1 app without Test::Unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Solitaire::Application.config.secret_token = 'b04ba853b9c63390797d420f7b39d5c882860dd11df64224219bc89dac604e0dce09962313552cc7a01e684d9987f8906168e8d287e6bfa7b3f3ca3044e689cb'
